86df96d66f14400962bfa11216bf51a145512815
logan <prosserlogan@github.com>
2025-07-25 14:10:23 -0400

    deleted repeated users bind key

--- models.py
+++ models.py
@@ -18,8 +18,6 @@ class User(db.Model):
     """User model for authentication."""
     __bind_key__ = 'users'
 
-    __bind_key__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
