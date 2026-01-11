a8fe9d1afc9ac6f7684affbd144391e5ed667d53
loganprosser <loganpros@gmail.com>
2026-01-11 16:16:30 -0500

    changed label

--- visualizaions.py
+++ visualizaions.py
@@ -26,9 +26,9 @@ def vis_home():
     viz_tiles = [
         {
             "title": "Eyescan Browser",
-            "description": "Browse eyescan artifacts by CM serial and scan date.",
+            "description": "Browse eyescans by CM serial and scan date.",
             "href": url_for("visualizations.eyescan_home"),
-            "icon": "bi-graph-up",  # optional if you're using Bootstrap Icons
+            "icon": "bi-graph-up",
         },
 
     ]
